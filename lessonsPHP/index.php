19d6ceb6a3368b5228ea2b00b2fcb0d6675acdf3
xkislotnikx <xkislotnikx@yhoo.com>
2017-10-01 17:43:19 +0300

    laba_2

--- lessonsPHP/index.php
+++ lessonsPHP/index.php
@@ -1,3 +1,11 @@
+<?php
+    //установка локали и даты
+setlocale(LC_ALL, "russian");
+$day = strftime('%d');
+$mon = strftime('%B');
+$mon = iconv('windows-1251','utf-8', $mon);
+$year = strftime('%y');
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
@@ -7,9 +15,19 @@
         <blockquote>
         <?php
         //вывод даты и времени
-        echo strftime('Сегодня %d-%m-%y');
+        //echo strftime('Сегодня %d-%m-%y');
+        echo 'Сегодня',$day,$mon,$year;
         ?>
         </blockquote>
-        
+
+        <?php
+        //определение констант
+        define("AAA","100");
+        const BBB=200;
+        echo AAA,'<br>';
+        echo BBB;
+        ?>
+        <?php
+        ?>
     </body>
 </html>
