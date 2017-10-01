8feb7218ee545176c5d6843080f8e720a7ff4790
xkislotnikx <xkislotnikx@yhoo.com>
2017-10-01 17:19:46 +0300

    laba_1

---
+++ lessonsPHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        
+        <blockquote>
+        <?php
+        //вывод даты и времени
+        echo strftime('Сегодня %d-%m-%y');
+        ?>
+        </blockquote>
+        
+    </body>
+</html>
